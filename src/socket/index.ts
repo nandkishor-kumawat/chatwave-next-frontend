7b39bb3e2f43593861134cc8eed3c24fdc0ef791
Nandkishor Kumawat <nandkishor41071@gmail.com>
2023-11-17 00:30:24 +0530

    Update index.ts

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,8 +1,9 @@
 import { io } from 'socket.io-client';
 
-const URL: string = 'https://elfin-brainy-balloon.glitch.me';
+// const URL: string = 'https://elfin-brainy-balloon.glitch.me';
 
-// const URL = 'https://chatwave-backend.vercel.app';
+const URL: string = 'https://chatwave-next-backend.onrender.com';
+// const URL: string = 'localhost:4000';
 const socket = io(URL, {
     autoConnect: false
 });
