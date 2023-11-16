87bfac3c9f4a8ee893e65463f04d49e60d09dc6a
Nandkishor Kumawat <nandkishor41071@gmail.com>
2023-11-17 00:30:18 +0530

    Update useChat.ts

--- src/custom hooks/useChat.ts
+++ src/custom hooks/useChat.ts
@@ -14,6 +14,7 @@ import {
 import { useAppDispatch, useAppSelector } from "@/redux/store";
 import { setCurrentUser } from "@/redux/features/userSlice";
 import { useSearchParams } from "next/navigation";
+import socket from "@/socket";
 
 
 export default function useChat() {
@@ -30,85 +31,54 @@ export default function useChat() {
     const name = params?.get('name')
 
 
-    // useEffect(() => {
-    //     const socketInitializer = async () => {
-    //         await fetch('/api/socket')
-    //         socketRef.current = io();
-
-    //         socketRef.current.on('connect', () => {
-    //             console.log('connected to socket', socketRef.current.id)
-    //             dispatch(setCurrentUser({ id: socketRef.current.id }))
-    //             socketRef.current.emit("newUser", name);
-    //         });
-
-    //         socketRef.current.on('newUserResponse', (users: User[]) => {
-    //             setOnlineUsers(users);
-    //             console.log(users)
-    //         });
-
-    //     }
-    //     socketInitializer()
-
-    //     if (socketRef.current) return socketRef.current.disconnect()
-    // }, [])
-
-
-
     useEffect(() => {
         if (!name) return;
+        console.log(socket.connected)
+        socket.connect()
 
-        // Disconnect the previous socket connection if it exists
-        if (socketRef.current) {
-            socketRef.current.disconnect();
-        }
-
-        fetch('/api/socket').finally(() => {
-            socketRef.current = io();
-
-            socketRef.current.on("connect", () => {
-                console.log(socketRef.current.id, 'in client');
-                dispatch(setCurrentUser({  id: socketRef.current.id }))
-                socketRef.current.emit("newUser", name);
-            });
-
-            socketRef.current.on('newUserResponse', (users: User[]) => {
-                setOnlineUsers(users);
-                console.log(users)
-            });
-
-            socketRef.current.on('message', (data: any) => {
-                setMessages((prev: any) => [...prev, data]);
-            });
-
-            socketRef.current.on('disconnect', () => {
-                console.log('disconnected--->', socketRef.current.id)
-            });
+        socket.on("connect", () => {
+            console.log(socket.id, 'in client');
+            dispatch(setCurrentUser({ id: socket.id }))
+            socket.emit("newUser", name);
         });
 
-        // Cleanup: Disconnect the socket when the component unmounts or when name changes
+        socket.on('newUserResponse', (users: User[]) => {
+            setOnlineUsers(users);
+            console.log(users)
+        });
+
+        socket.on('message', (data: any) => {
+            setMessages((prev: any) => [...prev, data]);
+        });
+
+        socket.on('disconnect', () => {
+            console.log('disconnected--->', socket.id)
+        });
+
+
         return () => {
-            if (socketRef.current) {
-                socketRef.current.disconnect();
+            if (socket) {
+                socket.disconnect();
             }
         };
-    }, [name]);
+    }, []);
 
 
 
     const userJoin = (name: any) => {
-        if (!socketRef.current) return;
+        if (!socket) return;
         console.log('sent')
-        socketRef.current.emit("newUser", name);
+        socket.emit("newUser", name);
     }
 
     const sendMessage = (data: any) => {
-        if (!socketRef.current) return;
-        socketRef.current.emit("message", data);
+        if (!socket) return;
+        socket.emit("message", data);
     }
 
     const changeRoom = (room: string) => {
-        if (!socketRef.current) return;
-        socketRef.current.emit("changeRoom", room);
+        if (!socket) return;
+        socket.emit("changeRoom", room);
     }
 
     return {
