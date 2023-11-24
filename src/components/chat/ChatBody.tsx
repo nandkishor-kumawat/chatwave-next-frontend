96e54fa358729e5394f3c34be8974c7cb208d88f
Nandkishor Kumawat <nandkishor41071@gmail.com>
2023-11-24 20:16:09 +0530

    Update ChatBody.tsx

--- src/components/chat/ChatBody.tsx
+++ src/components/chat/ChatBody.tsx
@@ -12,9 +12,9 @@ type PropTypes = {
 const ChatBody = ({ messages }: PropTypes) => {
     const secondUser = useAppSelector((state) => state.user.secondUser);
     const currentUser = useAppSelector((state) => state.user.currentUser);
-    
+
     const chatBodyRef = React.useRef<any>(null);
-    
+
     useEffect(() => {
         console.log(JSON.stringify(messages, null, 2))
         if (chatBodyRef.current) {
@@ -26,7 +26,7 @@ const ChatBody = ({ messages }: PropTypes) => {
     return (
         <Box className='flex-1 overflow-hidden'>
             <Box className="h-full relative">
-                <Box className="h-full w-full overflow-y-auto scrollbar"  ref={chatBodyRef}>
+                <Box className="h-full w-full overflow-y-auto scrollbar" ref={chatBodyRef}>
                     <Box className='flex flex-col px-2'>
                         {messages.filter((message: any) =>
                             (message.receiver_id === secondUser.email && message.sender_id === currentUser.email) ||
