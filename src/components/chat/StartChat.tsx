c4f6876c315e4c413679390153b5d460ee4a5a9c
Nandkishor Kumawat <nandkishor41071@gmail.com>
2023-11-25 22:05:53 +0530

    Create StartChat.tsx

---
+++ src/components/chat/StartChat.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+const StartChat = () => {
+  return (
+    <Box className='flex-1 flex items-center justify-center'>
+        <Typography variant='h5' className='text-white'>Start Chat</Typography>
+    </Box>
+  )
+}
+
+export default StartChat
