8c5ba01763b5dd439511ef951f9fecf35bc2a34c
Nandkishor Kumawat <nandkishor41071@gmail.com>
2023-11-25 22:05:39 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,19 @@
 'use client'
-// import socket from '@/socket';
-
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { useAppDispatch, useAppSelector } from '@/redux/store';
 import { auth } from '@/firebase';
 import { setCurrentUser } from '@/redux/features/userSlice';
 import ChatScreen from '@/components/ChatScreen';
 import { useEffect, useState } from 'react';
-import { Box } from '@mui/material';
-import Loader from '@/components/Loader';
+import LinearProgressBar from '@/components/progress/LinearProgressBar';
 
 
 export default function Home() {
 
   const currentUser = useAppSelector((state) => state.user.currentUser);
   const dispatch = useAppDispatch();
-  const router = useRouter()
+  const router = useRouter();
+  const [isUser, setIsUser] = useState(false);
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
@@ -27,18 +25,15 @@ export default function Home() {
         name: user.displayName,
         photoUrl: user.photoURL
       }))
-
     })
 
   }, [])
 
-  if (!currentUser) {
-    return <Loader/>
-  }
 
-
-
-
-
-  return <ChatScreen />
+  return (
+    <>
+      {!isUser && <LinearProgressBar onEnd={() => setIsUser(true)} />}
+      {currentUser && <ChatScreen />}
+    </>
+  )
 }
