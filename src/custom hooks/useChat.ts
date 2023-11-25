f814e77675a9f737d5e77f2fa640c3c3c03ab7b4
Nandkishor Kumawat <nandkishor41071@gmail.com>
2023-11-26 02:28:32 +0530

    Update useChat.ts

--- src/custom hooks/useChat.ts
+++ src/custom hooks/useChat.ts
@@ -109,6 +109,7 @@ export default function useChat() {
         onlineUsers,
         typingUsers,
         sendMessage,
-        changeRoom
+        changeRoom,
+        getSocket: () => socketRef.current
     };
 }      
