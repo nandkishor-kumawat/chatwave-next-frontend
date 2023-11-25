82abe6546eb3fdb0c3b22549ae79fca779671ec8
Nandkishor Kumawat <nandkishor41071@gmail.com>
2023-11-26 02:28:27 +0530

    Update ChatScreen.tsx

--- src/components/ChatScreen.tsx
+++ src/components/ChatScreen.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useEffect, useMemo } from 'react'
 import useChat from '@/custom hooks/useChat';
 import SideBar from '@/components/sidebar/SideBar';
-import { Box  } from '@mui/material';
+import { Box } from '@mui/material';
 import ChatContainer from '@/components/chat/ChatContainer';
 import { useAppSelector } from '@/redux/store';
 import StartChat from './chat/StartChat';
@@ -9,14 +9,17 @@ import StartChat from './chat/StartChat';
 const ChatScreen = () => {
 
     const secondUser = useAppSelector((state) => state.user.secondUser);
-
+    
     const {
         messages,
         onlineUsers,
         typingUsers,
         sendMessage,
-        changeRoom
+        changeRoom,
+        getSocket
     } = useChat();
+
+    const socket = useMemo(() => getSocket(), [getSocket]);
 
 
     return (
