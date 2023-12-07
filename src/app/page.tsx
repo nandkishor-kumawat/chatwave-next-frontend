29845391ddf49164990081b9120f7c3ffc16fbbc
Nandkishor Kumawat <nandkishor41071@gmail.com>
2023-12-07 23:39:25 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 'use client'
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 
 
 export default function Home() {
-  const router = useRouter();
+
 
   return (
     <>
